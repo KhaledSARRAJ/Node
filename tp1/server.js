6e99d10ca4bad34ac55c35c2172972d9c53fb08e
khaled <khaledinat@gmail.com>
2021-07-13 14:54:32 +0200

    commencer les bases js / creation d'un serveur

---
+++ tp1/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((rconst reponse2 = await fetch(url +'/'+datas[0].userId);
+const data = await reponse2.json();
+console.log('Promise: 2', data)eq,res)=>{
+res.writeHead(200,{'Content-type':'text/plain'})
+res.end('Hello word from NodeJS !!!');
+}).listen(8180, 'localhost');
+console.log('Server running on port 8180');
